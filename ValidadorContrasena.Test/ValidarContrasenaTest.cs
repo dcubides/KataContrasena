b166a697c40ff2e94de7ba8b0175ecedf8aaff2e
david.cubides <david.cubides@sinco.com.co>
2025-10-27 13:48:50 -0500

    🟢Si_ValidacionEs3Contrasena_Debe_Validar16CaracteresLetraMayusculaLetraMinusculaYUnGuionBajo

--- ValidadorContrasena.Test/ValidarContrasenaTest.cs
+++ ValidadorContrasena.Test/ValidarContrasenaTest.cs
@@ -184,7 +184,7 @@ public class ValidarContrasenaTest
         var validador = new ValidadorContrasena(reglas);
         
         //Act
-        bool contrasenaValida = validador.EsValida("Xxxx123");
+        bool contrasenaValida = validador.EsValida("Xxxx123_");
         
         //Assert
         contrasenaValida.Should().BeTrue();
